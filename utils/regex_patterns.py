423ae8503611476137cddc74f73b04aff0de1222
chiyodaluiz-blip <chiyoda.luiz@gmail.com>
2026-03-08 16:17:08 +0000

    Update regex_patterns.py

--- utils/regex_patterns.py
+++ utils/regex_patterns.py
@@ -2,10 +2,10 @@ DATE_REGEX = r"(\d{2}/\d{2}/\d{4})"
 
 TAX_REGEX = {
 
-"liquidacao": r"Taxa de liquidação\s+([\d,]+)",
-"registro": r"Taxa de registro\s+([\d,]+)",
-"emolumentos": r"Emolumentos\s+([\d,]+)",
-"operacional": r"Corretagem\s+([\d,]+)",
-"impostos": r"ISS\s+([\d,]+)"
+"liquidacao": r"Taxa de Liquidação\s+(-?[\d.,]+)",
+"registro": r"Taxa de Registro\s+(-?[\d.,]+)",
+"emolumentos": r"Emolumentos\s+(-?[\d.,]+)",
+"operacional": r"Corretagem\s+(-?[\d.,]+)",
+"impostos": r"ISS.*?\s(-?[\d.,]+)"
 
 }
