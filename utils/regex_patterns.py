7a616696f718e0420af3e4c492b829d6c5f98e68
chiyodaluiz-blip <chiyoda.luiz@gmail.com>
2026-03-09 19:57:49 +0000

    Update regex_patterns.py

--- utils/regex_patterns.py
+++ utils/regex_patterns.py
@@ -2,6 +2,8 @@ DATE_REGEX = r"Data\s*Preg[aã]o\s*(\d{2}/\d{2}/\d{4})"
 
 IRRF_REGEX = r"I\.R\.R\.F.*?([\d,]+)$"
 
+LIQUIDO_REGEX = r"L[ií]quido\s+para.*?(-?[\d.,]+)"
+
 TAX_REGEX = {
 
 "liquidacao": r"Taxa de Liquidação\s+(-?[\d.,]+)",
