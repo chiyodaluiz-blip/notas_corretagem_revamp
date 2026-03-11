f57da61be86a7c703521654189bc751b70098d55
chiyodaluiz-blip <chiyoda.luiz@gmail.com>
2026-03-11 15:11:44 +0000

    Update regex_patterns.py

--- utils/regex_patterns.py
+++ utils/regex_patterns.py
@@ -1,15 +1,15 @@
 DATE_REGEX = r"Data\s*Preg[aã]o\s*(\d{2}/\d{2}/\d{4})"
 
-IRRF_REGEX = r"I\.R\.R\.F.*?([\d,]+)$"
+IRRF_REGEX = r"I\.?R\.?R\.?F\.?[^\d]*?([\d.,]+)"
 
 LIQUIDO_REGEX = r"L[ií]quido\s+para.*?(-?[\d.,]+)"
 
 TAX_REGEX = {
 
-"liquidacao": r"Taxa de Liquidação\s+(-?[\d.,]+)",
-"registro": r"Taxa de Registro\s+(-?[\d.,]+)",
+"liquidacao": r"Taxa\s+de\s+Liquida[cç][aã]o\s+(-?[\d.,]+)",
+"registro": r"Taxa\s+de\s+Registro\s+(-?[\d.,]+)",
 "emolumentos": r"Emolumentos\s+(-?[\d.,]+)",
-"operacional": r"Corretagem\s+(-?[\d.,]+)",
-"impostos": r"ISS.*?\s(-?[\d.,]+)"
+"operacional": r"(?:Corretagem|Taxa\s+Operacional)\s+(-?[\d.,]+)",
+"impostos": r"ISS[^\d]*(-?[\d.,]+)"
 
 }
